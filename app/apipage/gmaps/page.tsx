e447b89e0de151962bbd097a5f4fec47dfbd6bd1
auziqni <auziqni@gmail.com>
2023-07-28 16:17:45 +0700

    feat: gmaps show
    just show google maps

--- app/apipage/gmaps/page.tsx
+++ app/apipage/gmaps/page.tsx
@@ -1,5 +1,38 @@
+"use client";
 import React from "react";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+
+const containerStyle = {
+  width: "400px",
+  height: "400px",
+};
+
+const center = {
+  lat: -3.745,
+  lng: -38.523,
+};
+// const ApiKey : string = process.env.GMAPS_API_KEY ;
 
 export default function page() {
-  return <div>page</div>;
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+
+    //todo taruh api di .env file
+    googleMapsApiKey: "AIzaSyBElwKJ9pelcp8nNwM9jiY7SutnLM38e_Y",
+  });
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={10}
+      //   onLoad={onLoad}
+      //   onUnmount={onUnmount}
+    >
+      {/* Child components, such as markers, info windows, etc. */}
+      <></>
+    </GoogleMap>
+  ) : (
+    <></>
+  );
 }
