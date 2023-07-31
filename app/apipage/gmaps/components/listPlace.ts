bfb828ffa8da7a060b2846b46afa60197641319f
auziqni <auziqni@gmail.com>
2023-07-31 15:06:48 +0700

    feat: multiple pointer

---
+++ app/apipage/gmaps/components/listPlace.ts
@@ -0,0 +1,52 @@
+export const listplaces = [
+  {
+    name: "bundaran 1",
+    lat: -5.361599,
+    lng: 105.22562,
+    val1: 1,
+    val2: 2,
+  },
+  {
+    name: "bundaran 2",
+    lat: -5.361626,
+    lng: 105.225769,
+    val1: 1,
+    val2: 2,
+  },
+  {
+    name: "bundaran 3",
+    lat: -5.361766,
+    lng: 105.225637,
+    val1: 1,
+    val2: 2,
+  },
+  {
+    name: "flyover 1",
+    lat: -5.365985,
+    lng: 105.230153,
+    val1: 1,
+    val2: 2,
+  },
+  {
+    name: "flyover 2",
+    lat: -5.366017,
+    lng: 105.230205,
+    val1: 1,
+    val2: 2,
+  },
+
+  {
+    name: "flyover 3",
+    lat: -5.365797,
+    lng: 105.230313,
+    val1: 1,
+    val2: 2,
+  },
+  {
+    name: "flyover 4",
+    lat: -5.36582,
+    lng: 105.230345,
+    val1: 1,
+    val2: 2,
+  },
+];
--- app/apipage/gmaps/components/maps.tsx
+++ app/apipage/gmaps/components/maps.tsx
@@ -3,9 +3,11 @@ import React, { useCallback, useMemo, useRef } from "react";
 
 import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
 
+import { listplaces } from "./listPlace";
+
 const containerStyle = {
-  width: "400px",
-  height: "400px",
+  width: "100%",
+  height: "500px",
 };
 
 const home = {
@@ -23,17 +25,22 @@ export default function Map() {
 
   //   const onload = useCallback((map) =>(mapRef.current=map,[]))
   return (
-    <GoogleMap
-      mapContainerStyle={containerStyle}
-      center={center}
-      zoom={10}
-      options={option}
-      //   onLoad={onLoad}
-      //   onUnmount={onUnmount}
-    >
-      {/* Child components, such as markers, info windows, etc. */}
+    <div>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+        options={option}
+        //   onLoad={onLoad}
+        //   onUnmount={onUnmount}
+      >
+        {/* Child components, such as markers, info windows, etc. */}
 
-      <Marker position={home} />
-    </GoogleMap>
+        <div></div>
+        {listplaces.map((listplace) => (
+          <Marker position={{ lat: listplace.lat, lng: listplace.lng }} />
+        ))}
+      </GoogleMap>
+    </div>
   );
 }
